6c72d78981e2627e15744ccef2f684509419325d
Munchurwoo <mdnbo1@naver.com>
2021-06-28 14:31:20 +0900

    DocM

--- aoppart2chapter02/app/src/main/java/com/example/aop/part2/aop_part2_chapter02/MainActivity.kt
+++ aoppart2chapter02/app/src/main/java/com/example/aop/part2/aop_part2_chapter02/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.aop.part2.aop_part2_chapter02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.NumberPicker
 
 class MainActivity : AppCompatActivity() {
+
+    private val addButton : Button by lazy {
+        findViewById<Button>(R.id.addButton)
+    }
+    private val clearButton : Button by lazy{
+        findViewById<Button>(R.id.clearButton)
+    }
+    private val runButton : Button by lazy{
+        findViewById<Button>(R.id.runButton)
+    }
+    private val numberPicker : NumberPicker by lazy{
+        findViewById<NumberPicker>(R.id.numberPicker)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
